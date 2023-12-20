e045b131d43d3cdb66807274ef5bc01c9999d5b7
Vivan-jha <vivaanjha2002@gmail.com>
2023-12-20 16:36:34 +0530

    first commit

--- src/jam_sdk/example.py
+++ src/jam_sdk/example.py
@@ -1,52 +1,31 @@
-import asyncio
+import requests
 
-from jam_sdk.solver.base import BaseSolver
-from jam_sdk.solver.types import (
-    CachedQuote,
-    ExecuteError,
-    ExecuteRequest,
-    ExecuteResponse,
-    InteractionData,
-    QuoteError,
-    QuoteRequest,
-    QuoteResponse,
-    SolverConnection,
-    TokenAmountResponse,
-)
+url = 'https://api.1inch.io/v5.2/1/quote'
+src_address = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+dst_address = '0x111111111117dc0aa78b770fa6a738034120c302'
+amount = '10000000000000000'
 
+# Build the parameters as a string and concatenate to the URL
+params_string = f'src={src_address}&dst={dst_address}&amount={amount}'
+full_url = f'{url}?{params_string}'
 
-class MySolver(BaseSolver):
-    async def get_quote(self, chain_id: int, request: QuoteRequest) -> QuoteResponse | QuoteError:
-        return QuoteResponse(
-            quote_id=request.quote_id,
-            amounts=[TokenAmountResponse("0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", 100000)],  # Output token amount
-            fee=50000000000000000,  # Fee in wei
-            executor="0x0000000000000000000000000000000000000000",
-        )
+# Make the GET request with the full URL
+response = requests.get(full_url)
 
-    async def execute(self, request: ExecuteRequest, quote_cache: CachedQuote) -> ExecuteResponse | ExecuteError:
-        interactions = [  # A set of calls to make on chain in order to fulfil the quote
-            InteractionData(
-                result=True, to="0x0000", value=0, data="0x00000000000"  # Whether the interaction must succeed
-            )
-        ]
-        account = self.web3.eth.account.from_key("<a private key>")
-        settle_tx = await self.build_settle(
-            quote_cache.request,
-            quote_cache.response,
-            interactions,
-            request,
-            self.jam_contract.address,  # The recipient of user sell tokens. Can be your own solver contract
-            {"from": account.address},
-        )
-        signed_transaction = account.sign_transaction(settle_tx)
-        await self.web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-        return ExecuteResponse(request.quote_id)
+# Print the entire response content, including headers
+print('Response Content:', response.text)
+print('Response Headers:', response.headers)
+print('Status Code:', response.status_code)
 
-
-if __name__ == "__main__":
-    connection = SolverConnection(
-        "my-solver", "mypassword123", "wss://api-test.bebop.xyz/jam/polygon/solver"
-    )  # Obtain from Bebop
-    solver = MySolver(chain_id=137, rpc_url="https://polygon-rpc.com", connection=connection)
-    asyncio.run(solver.start())
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    try:
+        # Try to decode the JSON content
+        json_response = response.json()
+        print('API Request Response:')
+        print(json_response)
+    except ValueError:
+        print('Invalid JSON content in the response.')
+else:
+    print('API Request Failed. Status Code:', response.status_code)
+    print('Error Details:', response.text)
