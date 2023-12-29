5670200d93b9e0c28ebbefa5a2f9ea065400cb95
Vivan-jha <vivaanjha2002@gmail.com>
2023-12-29 19:09:05 +0530

    1inch integration

--- src/jam_sdk/__init__.py
+++ src/jam_sdk/__init__.py
@@ -16,40 +16,69 @@ from jam_sdk.solver.types import (
 
 import os
 
-
 class QuoteRequest:
     def __init__(self, quote_id):
         self.quote_id = quote_id
+        
 
 class MySolver(BaseSolver):
     async def get_quote(self, chain_id: int, request: QuoteRequest) -> QuoteResponse | QuoteError:
-        print(f"sell token{request.sell_tokens}")
         print(f"Getting quote for chain {chain_id} with request: {request}")
+        sell_token_address = request.sell_tokens[0].address
+        sell_token_amount = request.sell_tokens[0].amount
+        buy_token_address = request.buy_tokens[0].address
+
+        # Your API call code
+        method = "get"
+        apiUrl = "https://api.1inch.dev/swap/v5.2/1/quote"
+        requestOptions = {
+            "headers": {
+                "Authorization": 'Bearer mjHMxye0xK3h8Zb3zz1iTu4HpKR4Chxy'
+            },
+            "body": {},
+            "params": {
+                "src": sell_token_address,
+                "dst": buy_token_address,
+                "amount": sell_token_amount ,
+                "includeTokensInfo": "true",
+                "includeProtocols": "true",
+                "includeGas": "true"
+            }
+        }
+
+        # Prepare request components
+        headers = requestOptions.get("headers", {})
+        body = requestOptions.get("body", {})
+        params = requestOptions.get("params", {})
+
+        response = requests.get(apiUrl, headers=headers, params=params)
+
+        # print("hiiiiiiiiiiiiiii I am 1inch api reponse" ,response.json())
+        response_data = response.json()
+        to_amount = response_data.get('toAmount')
+        to_token_address = response_data.get('toToken', {}).get('address')
+        gas_fee = response_data.get('gas')
+
+        # Your existing quote response logic
         quote_response = QuoteResponse(
             quote_id=request.quote_id,
-            amounts=[TokenAmountResponse("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1000000000000000000)],
-            fee=100,
+            amounts=[TokenAmountResponse(to_token_address, to_amount)],
+            fee=gas_fee,
             executor="0x5003F58BE3E0933559E6Ee00EfAb405C0F71E61F",
         )
 
         print(f"Quote response: {quote_response}")
-        username ='integrator'
-        password ='BWAqTtJmGRh9L2r66DUT'
-
-        # Now, use the obtained quote_id for the subsequent API call
-        dynamic_key1 = request.quote_id
-
-
+        
+        # Your subsequent API call using quote_id
+        quote_id = request.quote_id
+       
         url = "https://api-test.bebop.xyz/jam/ethereum/v1/order"
-
-
-
         data = {
-          "quote_id": dynamic_key1,
-          "sign_scheme": "EIP712"
+            "quote_id": quote_id,
+            "signature": "string",
+            "sign_scheme": "EIP712"
         }
-
-        response = requests.post(url, json=data,auth=(username, password))
+        response = requests.post(url, json=data, auth=("integrator", "BWAqTtJmGRh9L2r66DUT"))
 
         if response.status_code // 100 == 2:
             print("POST request successful")
@@ -61,7 +90,9 @@ class MySolver(BaseSolver):
         return quote_response
 
     private_key = os.environ.get("YOUR_PRIVATE_KEY")
+    
     async def execute(self, request: ExecuteRequest, quote_cache: CachedQuote) -> ExecuteResponse | ExecuteError:
+        # Your execution logic
         print(f"Executing request: {request}")
         interactions = [InteractionData(result=True, to="0x0000", value=0, data="0x00000000000")]
         account = self.web3.eth.account.from_key("YOUR_PRIVATE_KEY")
@@ -157,64 +188,229 @@ if __name__ == "__main__":
 
 
 
-# import asyncio
-# from jam_sdk.solver.base import BaseSolver
-# from jam_sdk.solver.types import (
-#     CachedQuote,
-#     ExecuteError,
-#     ExecuteRequest,
-#     ExecuteResponse,
-#     InteractionData,
-#     QuoteError,
-#     QuoteRequest,
-#     QuoteResponse,
-#     SolverConnection,
-#     TokenAmountResponse,
-# )
-
-# import os
-
-# class MySolver(BaseSolver):
-#     async def get_quote(self, chain_id: int, request: QuoteRequest) -> QuoteResponse | QuoteError:
-#         print(f"sell token{request.sell_tokens}")
-#         print(f"Getting quote for chain {chain_id} with request: {request}")
-#         quote_response = QuoteResponse(
-#             quote_id=request.quote_id,
-#             amounts=[TokenAmountResponse("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1000000000000000000)],
-#             fee=100,
-#             executor="0x5003F58BE3E0933559E6Ee00EfAb405C0F71E61F",
-#         )
-
-#         print(f"Quote response: {quote_response}")
-#         return quote_response
 
 
-    
-#     private_key = os.environ.get("YOUR_PRIVATE_KEY")
-#     async def execute(self, request: ExecuteRequest, quote_cache: CachedQuote) -> ExecuteResponse | ExecuteError:
-        
-#         print(f"Executing request: {request}")
-#         interactions = [InteractionData(result=True, to="0x0000", value=0, data="0x00000000000")]
-#         account = self.web3.eth.account.from_key("YOUR_PRIVATE_KEY")
-#         settle_tx = await self.build_settle(
-#             quote_cache.request,
-#             quote_cache.response,
-#             interactions,
-#             request,
-#             self.jam_contract.address,
-#             {"from": account.address},
-#         )
-#         print(f"Built settlement transaction: {settle_tx}")
-#         signed_transaction = account.sign_transaction(settle_tx)
-#         print(f"Signed transaction: {signed_transaction}")
-#         await self.web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-#         print(f"Transaction sent successfully.")
-#         return ExecuteResponse(request.quote_id)
 
-# if __name__ == "__main__":
-#     connection = SolverConnection(
-#         "velvet", "041f760e-a2a2-4d1e-a90c-af9642b99498", "wss://api-test.bebop.xyz/jam/ethereum/solver"
-#     )
-#     solver = MySolver(chain_id=137, rpc_url="https://polygon-rpc.com", connection=connection)
-#     print("Solver initialized.")
-#     asyncio.run(solver.start())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # import asyncio
+# # import requests
+# # from jam_sdk.solver.base import BaseSolver
+# # from jam_sdk.solver.types import (
+# #     CachedQuote,
+# #     ExecuteError,
+# #     ExecuteRequest,
+# #     ExecuteResponse,
+# #     InteractionData,
+# #     QuoteError,
+# #     QuoteRequest,
+# #     QuoteResponse,
+# #     SolverConnection,
+# #     TokenAmountResponse,
+# # )
+
+# # import os
+
+
+# # class QuoteRequest:
+# #     def __init__(self, quote_id):
+# #         self.quote_id = quote_id
+
+# # class MySolver(BaseSolver):
+# #     async def get_quote(self, chain_id: int, request: QuoteRequest) -> QuoteResponse | QuoteError:
+# #         print(f"sell token{request.sell_tokens}")
+# #         print(f"Getting quote for chain {chain_id} with request: {request}")
+# #         quote_response = QuoteResponse(
+# #             quote_id=request.quote_id,
+# #             amounts=[TokenAmountResponse("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 1000000000000000000)],
+# #             fee=100,
+# #             executor="0x5003F58BE3E0933559E6Ee00EfAb405C0F71E61F",
+# #         )
+
+# #         print(f"Quote response: {quote_response}")
+# #         username ='integrator'
+# #         password ='BWAqTtJmGRh9L2r66DUT'
+
+# #         # Now, use the obtained quote_id for the subsequent API call
+# #         dynamic_key1 = request.quote_id
+
+
+# #         url = "https://api-test.bebop.xyz/jam/ethereum/v1/order"
+
+
+
+# #         data = {
+# #           "quote_id": dynamic_key1,
+# #           "sign_scheme": "EIP712"
+# #         }
+
+# #         response = requests.post(url, json=data,auth=(username, password))
+
+# #         if response.status_code // 100 == 2:
+# #             print("POST request successful")
+# #             print("Response:", response.json())
+# #         else:
+# #             print(f"POST request failed with status code {response.status_code}")
+# #             print("Response:", response.text)
+
+# #         return quote_response
+
+# #     private_key = os.environ.get("YOUR_PRIVATE_KEY")
+# #     async def execute(self, request: ExecuteRequest, quote_cache: CachedQuote) -> ExecuteResponse | ExecuteError:
+# #         print(f"Executing request: {request}")
+# #         interactions = [InteractionData(result=True, to="0x0000", value=0, data="0x00000000000")]
+# #         account = self.web3.eth.account.from_key("YOUR_PRIVATE_KEY")
+# #         settle_tx = await self.build_settle(
+# #             quote_cache.request,
+# #             quote_cache.response,
+# #             interactions,
+# #             request,
+# #             self.jam_contract.address,
+# #             {"from": account.address},
+# #         )
+# #         print(f"Built settlement transaction: {settle_tx}")
+# #         signed_transaction = account.sign_transaction(settle_tx)
+# #         print(f"Signed transaction: {signed_transaction}")
+# #         await self.web3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+# #         print(f"Transaction sent successfully.")
+
+# #         return ExecuteResponse(request.quote_id)
+
+# # if __name__ == "__main__":
+# #     connection = SolverConnection(
+# #         "velvet", "041f760e-a2a2-4d1e-a90c-af9642b99498", "wss://api-test.bebop.xyz/jam/ethereum/solver"
+# #     )
+# #     solver = MySolver(chain_id=137, rpc_url="https://polygon-rpc.com", connection=connection)
+# #     print("Solver initialized.")
+# #     asyncio.run(solver.start())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
